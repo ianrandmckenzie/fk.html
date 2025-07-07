0469c544b3b7a18177a8f440538effa366581209
Curious Markings <exampleuser@Curiouss-MacBook-Air.local>
2025-07-06 18:19:42 -0700

    iPad tweaks

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ['./**/*.html', './main.js', './style.css'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        // iPad Pro specific breakpoints
+        'ipad-portrait': {'raw': '(min-width: 834px) and (max-width: 1024px) and (orientation: portrait)'},
+        'ipad-landscape': {'raw': '(min-width: 1024px) and (max-width: 1366px) and (orientation: landscape) and (max-height: 1024px)'},
+        'ipad-pro-landscape': {'raw': '(min-width: 1194px) and (max-width: 1366px) and (orientation: landscape) and (max-height: 834px)'},
+      }
+    },
   },
   plugins: [],
 }
